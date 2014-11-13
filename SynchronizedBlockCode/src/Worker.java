fe462fbdc556f3493337fc5609d3eabddca7b2e8
Димон <@192.168.1.101>
2014-11-13 12:02:09 +0200

    Synchronized Block Code Server Request

---
+++ SynchronizedBlockCode/src/Worker.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class Worker {
+	
+	private ArrayList<Integer> list1 = new ArrayList<Integer>();
+	private ArrayList<Integer> list2 = new ArrayList<Integer>();
+	private Object lock1 = new Object();
+	private Object lock2 = new Object();
+	private Random random = new Random();
+	
+		private void sendRequest(){
+		synchronized (lock1) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			list1.add(random.nextInt(100));
+		}
+	}
+
+	private void receiptAnswer(){
+		synchronized (lock2) {
+		try {
+			Thread.sleep(1);
+		} catch (InterruptedException e) {
+						e.printStackTrace();
+		}
+		list2.add(random.nextInt(100));
+		}
+	}
+	
+	private void proceed(){
+		for(int i=0; i<1000; i++){
+			sendRequest();
+			receiptAnswer();
+		}
+	}
+	
+	public void start(){
+		System.out.println("Begin!");
+		long startTime = System.currentTimeMillis();
+		
+		Thread t1 = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				proceed();				
+			}
+		});
+		
+		Thread t2 = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				proceed();				
+			}
+		});
+		
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("Elapsed Time: "+ (endTime-startTime)+"\n"
+		                   +"List1: "+list1.size() + "\n"
+				           +"List2: "+list2.size());
+		
+		
+		
+		
+		
+		
+		
+	}
+}
