20f7e54a7ad9a3db2582d5d27a34b433b040dc3c
Dmitry <uasmirnovd@gmail.com>
2014-11-18 11:35:50 +0200

    Collections

---
+++ Collections/src/ua/smirnov/Main.java
@@ -0,0 +1,18 @@
+package ua.smirnov;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
+	private static ArrayList<Mobile> mobiles = new ArrayList<Mobile>();
+	private static Random r = new Random();
+
+	public static void main(String[] args) {
+		for(int i=0; i<300; i++){
+			mobiles.add(new Mobile(r.nextInt(9999999), "This name"));
+		}
+		for(Mobile m: mobiles){
+			System.out.println(m.getNumber() + " "+ m.getName());
+		}
+		}
+	}
