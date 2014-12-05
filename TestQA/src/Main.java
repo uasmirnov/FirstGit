e35bd011150ca3e8c14264387ae2f12f1d4e9cce
Dmitry <utakagame@mail.ru>
2014-12-05 09:17:33 +0200

    Update Main.java

--- TestQA/src/Main.java
+++ TestQA/src/Main.java
@@ -8,17 +8,15 @@ public class Main {
 		System.out.println("Enter first variale");
 		int a = scn.nextInt();
 		
-		if (a == 0)
-		{
-			System.out.println("First variable = 0" + " Error");
+		if (a == 0) {
+			System.out.println("First variable = 0" + " Error!");
 			System.exit(0);
 			}
 		else
-	     	if(a == 1)
-				{
-				while (a<4)
+	     	        if(a == 1) {
+				while (a < 4)
 				a++;
-				}
+			}
 	     			else System.exit(0);
 	     	
 		Scanner scn2 = new Scanner(System.in);
@@ -26,7 +24,7 @@ public class Main {
 		int b = scn2.nextInt();
 			
 		System.out.println("Result: " + a/b);
-		}		
+	}		
 }
 
 
